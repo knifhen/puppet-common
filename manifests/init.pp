27df9e05f3a13e12693f2cfa15d6d7aad5aa62a3
Andreas Knifh <aknifh@williamsinteractive.com>
2015-04-28 12:13:55 +0200

    added common class

---
+++ manifests/init.pp
@@ -0,0 +1,40 @@
+class common {
+  include apt
+  include maven
+
+  apt::ppa { ['ppa:webupd8team/sublime-text-3', 'ppa:ubuntu-wine/ppa', 'ppa:webupd8team/java']:
+    release => 'trusty',
+    options => '-y';
+  }
+
+  apt::key { 'spotify': id => 'E22CBE98B5575C1857671254082CCEDF94558F59'; }
+
+  apt::source { 'spotify':
+    location => 'http://repository.spotify.com',
+    release  => 'stable',
+    repos    => 'non-free';
+  }
+
+  file { '/etc/apt/responsefile': source => 'puppet:///modules/common/responsefile'; }
+
+  package {
+    [
+      'vim',
+      'curl',
+      'iotop',
+      'sublime-text-installer',
+      'spotify-client',
+      'python-pip',
+      'python-gpgme',
+      'vagrant',
+      'virtualbox-4.3',
+      'wine1.7',
+      'oracle-java7-installer',
+      'daemon']:
+      responsefile => '/etc/apt/responsefile',
+      require      => Class['apt::update'];
+
+    ['r10k']:
+      provider => 'gem';
+  }
+}
